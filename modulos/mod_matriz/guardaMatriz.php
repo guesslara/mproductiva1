2807a23f8e1ea196c861f3869bdd78a84cfdbb41
guesslara <glara.styleweb@gmail.com>
2013-07-09 19:47:23 -0500

    Modificacion para Guardar

--- modulos/mod_matriz/guardaMatriz.php
+++ modulos/mod_matriz/guardaMatriz.php
@@ -5,10 +5,10 @@
         
         public function guardarMatriz($nombreArchivo,$idUsuario,$contenidoArchivo){
             //procedimiento para guardar el archivo en el directorio
-            $path="../../reportesUsuario/";
+            $path="../../reportesUsuario/".$nombreArchivo.".html";            
             if(is_dir($path)){
                 echo "procedimiento para guardar";
-                
+                $gestor=fopen($path,"r+");
             }else{
                 echo "El directorio especificado no es un Directorio Valido";
             }
